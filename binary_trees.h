f9ed1f46a41ca075e01324fcf88bf9ee9b26cc89
TechOgede <sammonius@live.com>
2023-06-03 08:59:19 -0700

    find the lowest common anceetor of two nodes

--- binary_trees.h
+++ binary_trees.h
@@ -23,8 +23,11 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
+/* Helper functions */
 void binary_tree_print(const binary_tree_t *);
+void launch_test(binary_tree_t *n1, binary_tree_t *n2);
 
+/* Functions for tasks */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -46,5 +49,30 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 size_t _pow(size_t base, size_t exp);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,\
+		const binary_tree_t *second);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif
